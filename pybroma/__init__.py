b22991d35b3299716dcca4959c26a4e17978e170
Calloc <140273795+CallocGD@users.noreply.github.com>
2024-03-12 14:05:30 -0600

    Update __init__.py

--- pybroma/__init__.py
+++ pybroma/__init__.py
@@ -1,1 +1,17 @@
 from .PyBroma import *
+from enum import IntEnum 
+
+class Platform(IntEnum):
+	"""Special Platform Number NOTE: 
+	  This hasn't been merged to cython yet due to 
+	  there being compatability issues with Broma 
+    itself because it has a flag called "None" which 
+    Violates Python's rules for enum names...
+  """
+	NONE = 0
+	Mac = 1
+	Windows = 2
+	Android = 4
+	iOS = 8
+	Android32 = 16
+	Android64 = 32
