9ab19cb306e4b40a53016b0b3d8a9388a9d00f11
Calloc <140273795+CallocGD@users.noreply.github.com>
2024-06-02 22:11:07 -0500

    Update Helper & upload ghc

--- pybroma/include/helper.hpp
+++ pybroma/include/helper.hpp
@@ -1,8 +1,8 @@
 #ifndef __HELPER_HPP__
 #define __HELPER_HPP__
 
-#include "broma.hpp"
-#include "ast.hpp"
+#include "Broma/include/broma.hpp"
+#include "Broma/include/ast.hpp"
 
 /* Used as a primary helpers around Field's variant and other Types since variants
 are not supported by cython yet as far as I am aware... */
@@ -19,15 +19,7 @@ inline broma::InlineField *Field_GetAs_InlineField(broma::Field *f)
     return nullptr;
 }
 
-inline broma::OutOfLineField *Field_GetAs_OutOfLineField(broma::Field *f)
-{
-    broma::OutOfLineField *x;
-    if (x = std::get_if<broma::OutOfLineField>(&f->inner))
-    {
-        return x;
-    };
-    return nullptr;
-}
+
 
 inline broma::FunctionBindField *Field_GetAs_FunctionBindField(broma::Field *f)
 {
@@ -92,5 +84,48 @@ broma::Class *RootgetClassFromStr(broma::Root *root, std::string const &name)
 {
     return root->operator[](name);
 }
+
+
+/* it all comes from python refusing to use the keyword "None" as 
+ * an enum The Broma Devs did not take kindly to my pull request 
+ * to fix  this None keyword to "NONE" so we have to translate this one 
+ * out ourselves as a workaround to the problem */
+
+enum class PyPlatform {
+    NONE = 0,
+    Mac = 1,
+	Windows = 2,
+	Android = 4,
+	iOS = 8,
+	Android32 = 16 | 4,
+	Android64 = 32 | 4, 
+	MacIntel = 64 | 1,
+	MacArm = 128 | 1,
+};
+
+/* @brief fixes Unacceptable/innapropreate names allowing for python to maintain compatability */
+PyPlatform fix_platformname(broma::Platform platform){
+
+    #define TRANSLATE_THIS_ENUM(NAME) case broma::Platform::NAME: return PyPlatform::NAME
+    switch (platform)
+    {
+    // Translate the problem Variable that python despises...
+    case broma::Platform::None:
+        return PyPlatform::NONE;
+    TRANSLATE_THIS_ENUM(Mac);
+    TRANSLATE_THIS_ENUM(Windows);
+    TRANSLATE_THIS_ENUM(Android);
+    TRANSLATE_THIS_ENUM(iOS);
+    TRANSLATE_THIS_ENUM(Android32);
+    TRANSLATE_THIS_ENUM(Android64);
+    TRANSLATE_THIS_ENUM(MacIntel);
+    TRANSLATE_THIS_ENUM(MacArm);
+    default:
+        return PyPlatform::NONE;
+    }
+    /* We don't need this enum after this point... */
+    #undef TRANSLATE_THIS_ENUM
+}
+
 
 #endif // __HELPER_HPP__
